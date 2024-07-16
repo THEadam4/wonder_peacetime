46e1f1d6562fee45a0670de8dab5ceebdcb48386
THEadam4 <113296364+THEadam4@users.noreply.github.com>
2024-07-16 19:06:18 +0200

    Delete wonder_peacetime_open directory

--- wonder_peacetime_open/locales/en.lua
+++
@@ -1,7 +0,0 @@
-Locales['en'] = {
-  ['no_PD'] = 'You cant turn off peactime when there is no PD/SD',
-  ['pt_reason'] = 'PEACETIME is now active! Reason: ',
-  ['pt_noreason'] = 'PEACETIME is now active!',
-  ['pt_disabled'] = 'PEACETIME is no longer active. :-)',
-  ['pt_reason'] = 'Důvod',
-}
